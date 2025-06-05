75082b2715f7f59691c855a28726c8fc20710d7e
VanillaKach <4d4m5@mail.ru>
2025-06-05 18:25:50 +0300

    Проект обновлен до домашнего задания 24. Добавлены файлы .html и обновлен README.md

--- django_store/urls.py
+++ django_store/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from catalog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', include('catalog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
